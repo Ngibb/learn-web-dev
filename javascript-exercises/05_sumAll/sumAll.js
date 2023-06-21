6f7d692e2f7d43897510d5a5bb3417063e5ced36
ngibb <ngibb94@gmail.com>
2023-06-20 20:07:26 -0700

    05 passed

---
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -0,0 +1,25 @@
+const sumAll = function(min, max) {
+    let sum = 0
+
+    if (!Number.isInteger(min) || !Number.isInteger(max) ) {
+        return 'ERROR'
+    }
+
+    if (min < 0 || max < 0) {
+        return 'ERROR'
+    }
+
+    if (max < min) {
+        [min, max] = [max, min]
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum
+
+};
+
+// Do not edit below this line
+module.exports = sumAll;
